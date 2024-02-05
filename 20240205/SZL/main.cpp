f780c3b337d26a609b1aab18e8328a2eb2525dd6
HighbornHellest <thehellestdk@gmail.com>
2024-02-05 17:48:34 +0100

    Update main.cpp
    a second solution

--- 20240205/SZL/main.cpp
+++ 20240205/SZL/main.cpp
@@ -15,7 +15,22 @@ int second_basic(std::vector<int> arr)
 
 int second_differnetly(std::vector<int> arr)
 {
+    int max=0, max2=0;
+    for(auto it : arr)
+    {
+        if(it > max )
+        {
+            max2=max;
+            max=it;
+        }
+        
+        if(it > max2 && it < max)
+        {
+            max2=it;
+        }
+    }
     
+    return max2;
 }
 
 int main()
@@ -24,7 +39,12 @@ int main()
     std::vector<int> vec = {3,2,1,4,5,6,9,8,11};
     int a = second_basic(vec);
     
+    int b = second_differnetly(vec);
+    
     std::cout<<a<<std::endl;
+    std::cout<<b<<std::endl;
+    
+    
 
     return 0;
 }
